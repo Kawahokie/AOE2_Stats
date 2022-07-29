a091f81a650623681c009d4653d5acdcbe40da39
Alan O <alan.overby@gmail.com>
2022-07-28 22:17:24 -0400

    Changed tuple to list. Revamped loading of list

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from os import path
 
 
 stats_Filename = "Stats.csv"
+statlist=[]
 with open('notes.json') as json_file:
     notes_list = json.load(json_file)
 
@@ -21,21 +22,40 @@ with open('data.json', 'w') as f:
 with open('data.json') as json_file:
     datafile = json.load(json_file)
 
+for game in datafile:
+    column = []
+    for player in game['players']:
+        if game['rating_type'] == 2 or game['rating_type'] == 4:
+            column.append(game['match_id'])
+            column.append(game['name'])
+            column.append(game['version'])
+            column.append(game['num_players'])
+            column.append(game['map_type'])
+            # column.append(notes_list['map_type'][game['map_type']-9]['string'])
+            column.append(notes_list['game_type'][game['game_type']]['string'])
+            column.append(notes_list['leaderboard'][game['leaderboard_id']]['string'])
+            column.append(notes_list['rating_type'][game['rating_type']]['string'])
+            column.append(player['name'])
+            column.append(player['rating'])
+            column.append(player['team'])
+            column.append(notes_list['civ'][player['civ']-1]['string'])
+            column.append(1 if player['won']==True else 0)
+            statlist.append(column)
+            # statdata = game['match_id'], game['name'], game['version'], game['num_players'], notes_list['map_type'][game['map_type']-9]['string'], notes_list['game_type'][game['game_type']]['string'], notes_list['leaderboard'][game['leaderboard_id']]['string'], notes_list['rating_type'][game['rating_type']]['string'], player['name'], player['rating'], player['team'], notes_list['civ'][player['civ']-1]['string'], 1 if player['won']==True else 0
+            print(column)
+            # writer.writerow(statdata)
+            
+
 if not path.exists(stats_Filename):
     with open(stats_Filename, 'a', newline='') as f:
         writer=csv.writer(f)
         headerdata = "match_id", "name", "version", "num_players", "map_type", "game_type", "leaderboard_id", "rating_type", "player_name", "elo", "team", "civ", "won"
         writer.writerow(headerdata)
 
-with open(stats_Filename, 'a', newline='') as f:
+with open(stats_Filename, 'a', newline='', encoding='utf-8-sig') as f:
     writer=csv.writer(f)
-    for game in datafile:
-        for player in game['players']:
-            if game['rating_type'] == 2 or game['rating_type'] == 4:
-                statdata = game['match_id'], game['name'], game['version'], game['num_players'], notes_list['map_type'][game['map_type']-9]['string'], notes_list['game_type'][game['game_type']]['string'], notes_list['leaderboard'][game['leaderboard_id']]['string'], notes_list['rating_type'][game['rating_type']]['string'], player['name'], player['rating'], player['team'], notes_list['civ'][player['civ']-1]['string'], 1 if player['won']==True else 0
-                writer.writerow(statdata)
-                # statdata = game['match_id'], game['name'], game['version'], game['num_players'], game['map_type'], game['game_type'], game['leaderboard_id'], game['rating_type'], player['name'], player['rating'], player['team'], player['civ'], player['won']
-                # writer.writerow(statdata)
+    writer.writerows(statlist)
+
 
 # Data to grab: match_id, name, version, num_players, 
 # map_type, game_type, leaderboard_id, rating_type
